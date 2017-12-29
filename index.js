61212076510cce9575eb6f3b637a5c0b4ce691d5
alextanhongpin <alextan@seekasia.com>
2017-12-29 15:16:48 +0800

    refactor: simplify the load function

--- index.js
+++ index.js
@@ -21,23 +21,23 @@ async function main () {
 
 function printTranslations (locale) {
   // Return translation by key
-  console.log(locale.t('good_day'))
+  console.log(locale('good_day'))
   console.log()
 
   // Return translation with dynamic variables
-  console.log(locale.t('greeting', { name: 'John' }))
+  console.log(locale('greeting', { name: 'John' }))
   console.log()
 
   // Translations by context
-  console.log(locale.t('homepage_url'))
-  console.log(locale.t('homepage_url', { context: 'malaysia' }))
-  console.log(locale.t('homepage_url', { context: 'indonesia' }))
+  console.log(locale('homepage_url'))
+  console.log(locale('homepage_url', { context: 'malaysia' }))
+  console.log(locale('homepage_url', { context: 'indonesia' }))
   console.log()
 
   // Nested objects in {locale}.json
-  console.log(locale.t('button.sign_up'))
-  console.log(locale.t('button.buy'))
-  console.log(locale.t('button.see_more'))
+  console.log(locale('button.sign_up'))
+  console.log(locale('button.buy'))
+  console.log(locale('button.see_more'))
   console.log()
 
   // Passing object in template, and format currency
@@ -49,23 +49,23 @@ function printTranslations (locale) {
       value: 100.0
     }
   }
-  console.log(locale.t('buy_coffee', { coffee }))
+  console.log(locale('buy_coffee', { coffee }))
   console.log()
 
   // Singular/Plural handling
-  console.log(locale.t('job_message', { count: 1 })) // Singular
-  console.log(locale.t('job_message', { count: 100 })) // Plural
+  console.log(locale('job_message', { count: 1 })) // Singular
+  console.log(locale('job_message', { count: 100 })) // Plural
   console.log()
 
   // Datetime formatting
   const date = { type: 'date', value: new Date() }
-  console.log(locale.t('today_message', { date }))
+  console.log(locale('today_message', { date }))
   console.log()
 
   // Example of context
-  console.log(locale.t('receive_message'))
-  console.log(locale.t('receive_message', { context: 'email' }))
-  console.log(locale.t('receive_message', { context: 'facebook' }))
+  console.log(locale('receive_message'))
+  console.log(locale('receive_message', { context: 'email' }))
+  console.log(locale('receive_message', { context: 'facebook' }))
 }
 
 main()
--- locale/index.js
+++ locale/index.js
@@ -3,15 +3,16 @@ const i18next = require('i18next')
 const moment = require('moment')
 const numeral = require('./numeral')
 
-async function Locale (lng = 'en-SG') {
+// Load resources
+const english = require('./resource/en-SG.json')
+const german = require('./resource/de.json')
+const indonesia = require('./resource/id.json')
+
+async function Locale (lng, resource) {
   const options = {
-    lng, // Default locale
-    fallbackLng: 'en-SG', // Fallback to english locale
-    debug: false,
+    lng,
     resources: {
-      'en-SG': require('./resource/en-SG.json'),
-      id: require('./resource/id.json'),
-      de: require('./resource/de.json')
+      [lng]: resource
     },
     // Add interpolation for moment.js, numeral.js and intl api
     interpolation: {
@@ -29,24 +30,10 @@ async function Locale (lng = 'en-SG') {
     }
   }
 
-  // There are severals events that you can subscribe to.
-  // i18next.on('initialized', () => console.log('i18next:initialized\n'))
-  // i18next.on('loaded', () => console.log('i18next:loaded\n'))
-  // i18next.on('failedLoading', () => console.log('i18next:failedLoading\n'))
-  // i18next.on('missingKey', () => console.log('i18next:missingKey\n'))
-  // i18next.on('added', () => console.log('i18next:added\n'))
-  // i18next.on('removed', () => console.log('i18next:removed\n'))
-
   return new Promise((resolve, reject) => {
     const instance = i18next.createInstance()
-    // Don't change locale globally - it will affect other translations
-    // instance.on('languageChanged', (lng) => {
-    //   console.log(`i18next:languageChanged => ${lng}\n`)
-    //   moment.locale(lng)
-    //   numeral.locale(lng)
-    // })
     instance.init(options, (error, t) => {
-      error ? reject(error) : resolve(instance)
+      error ? reject(error) : resolve(t)
     })
   })
 }
@@ -54,12 +41,12 @@ async function Locale (lng = 'en-SG') {
 // Create new instances of locales to avoid race condition - when serving multiple requests,
 // changing the locale will affect other requests that is performing the translation
 async function MultiLocale () {
-  const locales = [
-    { 'en-SG': await Locale('en-SG') },
-    { id: await Locale('id') },
-    { de: await Locale('de') }
-  ]
-  const translations = locales.reduce((a, b) => Object.assign(a, b), {})
+  const translations = {
+    'en-SG': await Locale('en-SG', english),
+    id: await Locale('id', indonesia),
+    de: await Locale('de', german)
+  }
+
   return {
     get (lng) {
       const translation = translations[lng]
